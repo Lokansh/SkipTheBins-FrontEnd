a5ba4c1339c715e441c51faa18ce4be131e46df3
Aabhaas Jain <aabhaas@Aabhaass-MacBook-Air.local>
2022-02-23 23:58:10 -0400

    initial code

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        Skip The Bins
+        </p>
+        <a
+          className="App-link"
+          href="https://git.cs.dal.ca/aabhaas/group_14_csci5709"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Group 14
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
