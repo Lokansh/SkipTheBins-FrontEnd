35c915bcea0768cae9dccf9bf7a18f83d0752b31
Lokansh Gupta <lokanshgupta@gmail.com>
2022-03-29 01:25:49 -0300

    Commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
-import './App.css';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
+import "./App.css";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 import Faq from "./components/faq/Faq";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
@@ -18,17 +17,11 @@ import ConfirmSchedule from "./components/Pickups/Vendor/ConfirmSchedule";
 import ViewSchedule from "./components/Pickups/Vendor/ViewSchedule";
 import DeleteSchedule from "./components/Pickups/Vendor/DeleteSchedule";
 import EditSchedule from "./components/Pickups/Vendor/EditSchedule";
-=======
-import "./App.css";
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
-import Faq from "./components/faq/Faq";
+
 import ContactUs from "./components/contactUs/ContactUs";
 import SubmitQueryForm from "./components/contactUs/SubmitQueryForm";
 import AdminDisplayQueries from "./components/contactUs/AdminDisplayQueries";
 import AdminModifyVendors from "./components/contactUs/AdminModifyVendors";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
->>>>>>> 3df3b7ed (static frontend)
 
 function App() {
   return (
@@ -37,10 +30,20 @@ function App() {
       <div className="m-4 body-container">
         <Router>
           <Routes>
-<<<<<<< HEAD
-            <Route path='/faq' exact element={<Faq editMode={false} role='user' />} ></Route>
-            <Route path='/faq-vendor'  element={<Faq editMode={true} role='vendor' />} ></Route>
-            <Route path='/faq-admin' exact element={<Faq editMode={true} role='admin' />} ></Route>
+            <Route
+              path="/faq"
+              exact
+              element={<Faq editMode={false} role="user" />}
+            ></Route>
+            <Route
+              path="/faq-vendor"
+              element={<Faq editMode={true} role="vendor" />}
+            ></Route>
+            <Route
+              path="/faq-admin"
+              exact
+              element={<Faq editMode={true} role="admin" />}
+            ></Route>
             <Route path="/user/pickups">
               <Route
                 path="/user/pickups/schedule"
@@ -81,8 +84,6 @@ function App() {
               ></Route>
               <Route index element={<PickupHomeVendor />}></Route>
             </Route>
-=======
-            <Route path="/faq" element={<Faq editMode={true} />}></Route>
             <Route path="/contactus">
               {/* <Route path="/submitquery" element={<SubmitQueryForm />}></Route> */}
               <Route index element={<ContactUs />}></Route>
@@ -99,7 +100,6 @@ function App() {
               path="/contactus/modifyvendors"
               element={<AdminModifyVendors />}
             ></Route>
->>>>>>> 3df3b7ed (static frontend)
           </Routes>
         </Router>
       </div>
